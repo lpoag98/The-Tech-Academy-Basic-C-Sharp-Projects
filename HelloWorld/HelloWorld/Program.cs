2218a8bce88f213f9662e04866cd4f1f92b7c816
Luke Poag <119906569+lpoag98@users.noreply.github.com>
2023-01-31 16:02:30 -0700

    update

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        //These two lines of code display the first text the user will see.
+        Console.WriteLine("The Tech Academy");
+        Console.WriteLine("Student Daily Report");
+        
+        //These lines of code display a prompt to be answered to with the readline()
+        Console.WriteLine("Please input your name:");
+        string name1 = Console.ReadLine();
+
+        Console.WriteLine("What course are you on?");
+        string course1 = Console.ReadLine();
+
+        //Convert.ToInt32 converts the valuetype 
+        Console.WriteLine("What page number?");
+        int pagenumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Do you need help with anything, please enter true or false.");
+        bool needhelp = Convert.ToBoolean(Console.ReadLine());
+        Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+
+        //This makes sure the program does not close at the end
+        Console.ReadLine();
+        
+    }
+}
+
---
+++ acme_accounting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        string myName = "jesse";
+        Console.WriteLine("Welcome to Acme Accounting Systems\nRemember, we're \"Accounting\" on you!");
+        Console.ReadLine();
+    }
+}
+

a3bcf5d6cc4de0cf21bee9353bc44790d70aa283
Luke Poag <119906569+lpoag98@users.noreply.github.com>
2023-02-03 11:47:57 -0700

    update 2

--- Branching_Practice/Branching_Practice/Program.cs
+++ Branching_Practice/Branching_Practice/Program.cs
@@ -11,19 +11,32 @@ namespace Branching_Practice
             Console.WriteLine("Please enter the package weight: ");
             int weight = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Please enter the package width: ");
-            int width = Convert.ToInt32(Console.ReadLine());
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            else
+            {
+                Console.WriteLine("Please enter the package width: ");
+                int width = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Please enter the package height: ");
-            int height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package height: ");
+                int height = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Please enter the package length: ");
-            int length = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package length: ");
+                int length = Convert.ToInt32(Console.ReadLine());
 
-            decimal total = weight * width * height * length / 100;
+                if (width + length + height > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+                else
+                { 
+                    decimal total = weight * width * height * length / 100;
 
-            Console.WriteLine("Your estimated total for shipping this package is: $" + total.ToString("0.00"));
-
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + total.ToString("0.00"));
+                }
+            }
 
             Console.WriteLine("Thank You!");
 

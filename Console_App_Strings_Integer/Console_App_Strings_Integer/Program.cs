65f31c251d5e7f911f79ad16e94f7c83c8c722e9
Luke Poag <119906569+lpoag98@users.noreply.github.com>
2023-02-05 20:50:37 -0700

    Assignment

---
+++ Console_App_Strings_Integer/Console_App_Strings_Integer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Console_App_Strings_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int> { 10, 20, 30, 40, 50 };
+
+            Console.WriteLine("Pick a number to divide each number in the list by: ");
+
+            //This is the try of the try-catch
+            try
+            {
+                int divisor = int.Parse(Console.ReadLine());
+                foreach (int number in intList)
+                {
+                    int result = number / divisor;
+                    Console.WriteLine(number + " divided by " + divisor + " = " + result);
+                }
+
+            //the catch block will catch any errors specified in the curly brackests
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Error: Cannot divide by zero.");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error: Invalid input. A number was expected.");
+            }
+
+            //always performed no matter what
+            finally
+            {
+                Console.ReadLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exception_Handling/Exception_Handling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a try catch block, here at the top is code to be tried. at the bottom where it says catch is where it catches the exceptions and handles them according to how its coded.
+            try
+            {
+                Console.WriteLine("Pick a number.");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number.");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two...");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+            
+        Console.ReadLine();
+        }
+    }
+

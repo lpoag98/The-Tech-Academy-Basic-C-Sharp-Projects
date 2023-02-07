37f8df3e049dbb1bddeb05475921ac0cc111a03e
Luke Poag <119906569+lpoag98@users.noreply.github.com>
2023-02-06 19:03:13 -0700

    assignment

---
+++ Method_Assignment/Method_Assignment/MathOperation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Assignment
+{
+    class MathOperation
+    {
+        // Method to do math operation with two integers
+        public int DoMath(int num1, int num2)
+        {
+            // Example math operation: return sum of two numbers
+            return num1 + num2;
+        }
+
+        // Method to do math operation with one integer
+        public int DoMath(int num1)
+        {
+            // Example math operation: return square of the number
+            return num1 * num1;
+        }
+    }
+}
---
+++ Method_Assignment/Method_Assignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the MathOperation class
+            MathOperation mathOp = new MathOperation();
+
+            // Ask user to input first number
+            Console.WriteLine("Enter first number: ");
+
+            // Converting the user's input from string to int
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask user to input second number (optional)
+            Console.WriteLine("Enter second number (optional): ");
+            string input = Console.ReadLine();
+            int num2 = 0;
+
+            // Checking if the user entered something for the second number
+            if (!string.IsNullOrEmpty(input))
+            {
+                num2 = Convert.ToInt32(input);
+            }
+
+            int result;
+            if (num2 != 0)
+            {
+                // If the second number was entered, calling the DoMath method with two parameters
+                result = mathOp.DoMath(num1, num2);
+            }
+            else
+            {
+                // If the second number was not entered, calling the DoMath method with one parameter (num1 * num1)
+                result = mathOp.DoMath(num1);
+            }
+
+            Console.WriteLine("Result: " + result);
+            Console.ReadLine();
+        }
+    }
+}

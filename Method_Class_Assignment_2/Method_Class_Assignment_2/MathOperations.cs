16b772c644289db99a6b4c8ec6e73dcdafc1e709
Luke Poag <119906569+lpoag98@users.noreply.github.com>
2023-02-06 22:32:16 -0700

    Method_Class2

---
+++ Method_Class_Assignment_2/Method_Class_Assignment_2/MathOperations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Class_Assignment_2
+{
+    public class MathOperations
+    {
+        // Method that takes two integers as parameters and performs a math operation on the first integer
+        public void DoOperation(int num1, int num2)
+        {
+            int result = num1 * num2; // perform math operation on num1
+            Console.WriteLine("The second integer is: " + num2); // display num2 to the screen
+        }
+    }
+}
---
+++ Method_Class_Assignment_2/Method_Class_Assignment_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Method_Class_Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations mathOp = new MathOperations(); // instantiate the class
+
+            mathOp.DoOperation(5, 10); // call the method and pass in two numbers
+            mathOp.DoOperation(num2: 20, num1: 5); // call the method and specify the parameters by name
+
+            Console.ReadLine();
+        }
+    }
+}

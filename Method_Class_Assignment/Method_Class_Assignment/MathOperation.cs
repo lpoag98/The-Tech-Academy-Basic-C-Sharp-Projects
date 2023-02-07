72b49d83ad6993ece163620b98415f029fddb3a5
Luke Poag <119906569+lpoag98@users.noreply.github.com>
2023-02-06 19:31:05 -0700

    Assignment

---
+++ Method_Class_Assignment/Method_Class_Assignment/MathOperation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Class_Assignment
+{
+    class MathOperation
+    {
+        // Method that takes two integers as parameters and performs a math operation on the first integer, then displays the second integer to the screen
+        public void DoMath(int firstInt, int secondInt)
+        {
+            int result = firstInt + 10;
+            Console.WriteLine("Result: " + result);
+            Console.WriteLine("Second Integer: " + secondInt);
+        }
+    }
+}
---
+++ Method_Class_Assignment/Method_Class_Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Method_Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation mathOp = new MathOperation();
+
+            Console.WriteLine("Enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Call method by passing two numbers as arguments
+            mathOp.DoMath(num1, num2);
+
+            // Call method by specifying parameters by name
+            mathOp.DoMath(secondInt: num2, firstInt: num1);
+
+            Console.ReadLine();
+        }
+    }
+}

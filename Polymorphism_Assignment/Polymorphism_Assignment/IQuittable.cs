82f1857beeb3ac473bf1a4ebbc47ad7f36ded305
Luke Poag <119906569+lpoag98@users.noreply.github.com>
2023-02-07 22:43:54 -0700

    Update

---
+++ Polymorphism_Assignment/Polymorphism_Assignment/IQuittable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism_Assignment
+{
+    interface IQuittable
+    {
+        void Quit();
+    }
+
+    // Employee class that implements IQuittable
+    class Employee : IQuittable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Implementation of Quit method
+        public void Quit()
+        {
+            Console.WriteLine("Employee quitting.");
+        }
+    }
+}
---
+++ Polymorphism_Assignment/Polymorphism_Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Polymorphism_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of type Employee
+            Employee employee = new Employee();
+
+            // Assign FirstName and LastName properties
+            employee.FirstName = "John";
+            employee.LastName = "Doe";
+
+            // Create an object of type IQuittable and assign the employee object to it
+            IQuittable quittable = employee;
+
+            // Call the Quit method on the IQuittable object
+            quittable.Quit();
+        }
+    }
+}

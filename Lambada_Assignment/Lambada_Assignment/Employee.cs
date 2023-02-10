f9d837d9a3142661009fd5ce7ab9d2b45c110ee4
Luke Poag <119906569+lpoag98@users.noreply.github.com>
2023-02-09 17:49:45 -0700

    update

---
+++ Lambada_Assignment/Lambada_Assignment/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lambada_Assignment
+{
+    class Employee
+    {
+        // Properties of the Employee class
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Lambada_Assignment/Lambada_Assignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lambada_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a list of 10 employees
+            List<Employee> employees = new List<Employee>()
+        {
+            new Employee { Id = 1, FirstName = "John", LastName = "Doe" },
+            new Employee { Id = 2, FirstName = "Jane", LastName = "Doe" },
+            new Employee { Id = 3, FirstName = "Joe", LastName = "Smith" },
+            new Employee { Id = 4, FirstName = "Jim", LastName = "Brown" },
+            new Employee { Id = 5, FirstName = "Jane", LastName = "Smith" },
+            new Employee { Id = 6, FirstName = "Joe", LastName = "Doe" },
+            new Employee { Id = 7, FirstName = "Jill", LastName = "Johnson" },
+            new Employee { Id = 8, FirstName = "Jack", LastName = "Brown" },
+            new Employee { Id = 9, FirstName = "Janet", LastName = "Smith" },
+            new Employee { Id = 10, FirstName = "Jim", LastName = "Johnson" }
+        };
+
+            // Use a foreach loop to create a new list of employees with the first name "Joe"
+            List<Employee> joes = new List<Employee>();
+            foreach (Employee employee in employees)
+            {
+                if (employee.FirstName == "Joe")
+                {
+                    joes.Add(employee);
+                }
+            }
+
+            // Use a lambda expression to create a new list of employees with the first name "Joe"
+            List<Employee> joesLambda = employees.FindAll(e => e.FirstName == "Joe");
+
+            // Use a lambda expression to create a new list of employees with an Id number greater than 5
+            List<Employee> idGreaterThan5 = employees.FindAll(e => e.Id > 5);
+
+            Console.ReadLine();
+        }
+    }
+}

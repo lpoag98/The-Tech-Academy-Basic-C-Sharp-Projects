ae45b01e88c06d0a66720206b016192cc3ffc095
Luke Poag <119906569+lpoag98@users.noreply.github.com>
2023-02-21 15:44:46 -0700

    Final

---
+++ Final_Challange/Final_Challange/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Final_Challange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new StudentContext("StudentDatabase"))
+            {
+                context.Seed();
+                Console.WriteLine("Student added to database.");
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Final_Challange/Final_Challange/StudentContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Challange
+{
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public StudentContext(string connectionString) : base(connectionString)
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public void Seed()
+        {
+            var student = new Student { Name = "John Doe", Age = 18 };
+            Students.Add(student);
+            SaveChanges();
+        }
+    }
+}
